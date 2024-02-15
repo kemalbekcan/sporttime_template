0a33042a26e83590f24a8d269503e563235398d0
kemalbekcan <kemal.bekcan@akinon.com>
2024-02-16 00:20:19 +0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,56 @@
-const { series, src, dest } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
 
-function compileSass() {
-    return src('scss/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(dest('styles/'));
-  }
+function htmlTask() {
+  return src('src/*.html')
+    .pipe(dest('public'));
+}
 
-exports.default = series(compileSass);
+// Sass Task
+function scssTask() {
+  return src('src/scss/app.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('public/css', { sourcemaps: '.' }));
+}
+
+// JavaScript Task
+function jsTask() {
+  return src('src/js/script.js', { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest('public/js', { sourcemaps: '.' }));
+}
+
+function browsersyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: './public'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['src/**/*.scss', 'src/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(
+  htmlTask,
+  scssTask,
+  jsTask,
+  browsersyncServe,
+  watchTask
+);
+
