0ece1eb29275cc4465f278a4e61008f74bcce3a0
Kemal Bekcan <kemal.bekcan@gmail.com>
2024-03-05 17:03:34 +0300

    initial commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 console.log('hello world');
+
+const mobileNav = document.querySelector('.mobile-header__navbar__top__hamburger-icon');
+const toggleNav = document.querySelector('.mobile-header__navbar__bottom');
+
+mobileNav.addEventListener('click', () => {
+    console.log('clicked')
+
+    toggleNav.classList.toggle('active');
+
+})
