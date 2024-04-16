d4a57513f49e64e4e38a4607df4b4f1ff2d1a062
Kemal Bekcan <kemalbekcan@gmail.com>
2024-04-16 15:38:00 +0300

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -30,18 +30,18 @@ function jsTask() {
 
 // JavaScript Task
 function imgTask() {
-  return src("src/img/*{jpg,png,gif,svg}")
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.mozjpeg({ quality: 75, progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
-        imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-        }),
-      ])
-    )
-    .pipe(dest("public/img"));
+  return src("src/img/*{jpg,png,gif,svg}").pipe(dest("public/img"));
+    // .pipe(
+    //   imagemin([
+    //     imagemin.gifsicle({ interlaced: true }),
+    //     imagemin.mozjpeg({ quality: 75, progressive: true }),
+    //     imagemin.optipng({ optimizationLevel: 5 }),
+    //     imagemin.svgo({
+    //       plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+    //     }),
+    //   ])
+    // )
+    
 }
 
 function browsersyncServe(cb) {
--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
-console.log('hello world');
+console.log("hello world");
 
-const mobileNav = document.querySelector('.mobile-header__navbar__top__hamburger-icon');
-const toggleNav = document.querySelector('.mobile-header__navbar__bottom');
+const mobileNav = document.querySelector(
+  ".mobile-header__navbar__top__hamburger-icon"
+);
+const toggleNav = document.querySelector(".mobile-header__navbar__bottom");
 
-mobileNav.addEventListener('click', () => {
-    console.log('clicked')
+mobileNav.addEventListener("click", () => {
+  console.log("clicked");
 
-    toggleNav.classList.toggle('active');
-
-})
+  toggleNav.classList.toggle("active");
+});
